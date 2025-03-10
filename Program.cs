31eb9f5d338916f4200b8835d240f170625793e6
mbougar <146754886+mbougar@users.noreply.github.com>
2025-03-10 01:07:54 +0100

    Comments

--- Program.cs
+++ Program.cs
@@ -12,22 +12,16 @@ using Api_Godot_PSP.MongoDb;
 using Api_Godot_PSP.Services;
 var builder = WebApplication.CreateBuilder(args);
 
-// Configurar servicios de MongoDB
 builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDB"));
 builder.Services.AddSingleton<MongoDbService>();
 
-// Configurar controladores y opciones de serialización JSON
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
 });
 
-// Configurar otros servicios necesarios aquí
-// builder.Services.Add... (si agregas más servicios en el futuro)
-
 var app = builder.Build();
 
-// Configuración de Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
@@ -36,8 +30,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
-// Mapeo de controladores (este es el que conecta las rutas con los controladores)
 app.MapControllers();
 
-// Ejecutar la aplicación
 app.Run();
